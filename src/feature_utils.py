6fecee961ea0d4652d50bf7d9583c296fdafa1a1
jfinn030-prog <jfinn030@gmail.com>
2026-04-07 17:22:53 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -126,9 +126,9 @@ def convert_input_pca_regression(request_body, request_content_type):
         return_period = 5
 
         SP500_1 = 'AOS_CR_Cum'
-        IBM_CR_Cum = json.loads(request_body)[SP500_1]
+        AOS_CR_Cum = json.loads(request_body)[SP500_1]
         SP500_2 = 'ABBV_CR_Cum'
-        NVDA_CR_Cum = json.loads(request_body)[SP500_2]
+        ABBV_CR_Cum = json.loads(request_body)[SP500_2]
 
         X = np.log(dataset.drop([target],axis=1)).diff(return_period)
         X = np.exp(X).cumsum()
