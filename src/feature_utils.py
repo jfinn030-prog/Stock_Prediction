5352c4887ab3bc16ce594bd6e9314aafbed2207d
jfinn030-prog <jfinn030@gmail.com>
2026-04-07 17:38:51 -0500

    Reorganize imports and clean up feature extraction

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -121,7 +121,8 @@ def convert_input_pca_regression(request_body, request_content_type):
         closest_row[techIndicator_2] = MOM_15
     
         return closest_row
-    else:
+
+        else:
 
         return_period = 5
 
@@ -130,21 +131,20 @@ def convert_input_pca_regression(request_body, request_content_type):
         SP500_2 = 'ABBV_CR_Cum'
         ABBV_CR_Cum = json.loads(request_body)[SP500_2]
 
-        X = np.log(dataset.drop([target],axis=1)).diff(return_period)
+        X = np.log(dataset.drop([target], axis=1)).diff(return_period)
         X = np.exp(X).cumsum()
         X.columns = [name + "_CR_Cum" for name in X.columns]
-        
-        # Calculate the distance
+
         distances = np.sqrt(
-            (X[SP500_1] - AOS_CR_Cum)**2 + 
+            (X[SP500_1] - AOS_CR_Cum)**2 +
             (X[SP500_2] - ABBV_CR_Cum)**2
         )
-        
+
         closest_index = distances.idxmin()
-        closest_row = X.loc[[closest_index]]
-    
+        closest_row = X.loc[[closest_index]].copy()
+
         closest_row[SP500_1] = AOS_CR_Cum
         closest_row[SP500_2] = ABBV_CR_Cum
-    
+
         return closest_row
     
