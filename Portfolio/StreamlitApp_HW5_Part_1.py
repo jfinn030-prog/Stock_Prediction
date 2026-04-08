0799de3730103cd50326cc349266d6a8b19583da
jfinn030-prog <jfinn030@gmail.com>
2026-04-07 19:30:07 -0500

    Refactor Streamlit app for improved functionality

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -114,12 +114,13 @@ def display_explanation(input_df, session, aws_bucket):
     input_df = convert_input_pca_regression(raw_json_input, 'application/json')
 
     best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
-    
-    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:2]) 
-    input_df_transformed = preprocessing_pipeline.transform(input_df) 
-    feature_names = best_pipeline[0:2].get_feature_names_out() 
-    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names) 
-    shap_values = explainer(input_df_transformed) 
+
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:3])
+    input_df_transformed = preprocessing_pipeline.transform(input_df)
+    n_cols = input_df_transformed.shape[1]
+    feature_names = [f'KernelPCA_{i+1}' for i in range(n_cols)]
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    shap_values = explainer(input_df_transformed)
   
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
